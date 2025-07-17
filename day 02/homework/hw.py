69414143f4ffb7cd9015655f3023a42aeea7e390
Temo2121 <temodash25@gmail.com>
2025-07-17 18:47:44 +0400

    Update hw.py

---
+++ day 02/homework/hw.py
@@ -0,0 +1,37 @@
+num1 = 4
+num2 = 2
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
+name1 = "temo"
+name2 = "saba"
+name3 = "gio"
+name4 = "dato"
+print(name1)
+num1 = 1
+num2 = 2 
+num3 = 3 
+num4 = 4
+print(num2)
+height1 = 1,75
+height2 = 1,74
+height3 = 1,73
+height4 = 1,72
+print(height4)
+age  = 1 
+age1 = 2 
+age2 = 3 
+age4 = 4  
+age5 = 5
+print(age+age1+age2+age4+age5) 
+ 
+height9 = 2
+age9 = 1,5
+print(height9*age9) 
+print(height9+age9)
+print(height9/age9)
+som = 1,5 , 1,5
+somt = error
+somth = error
