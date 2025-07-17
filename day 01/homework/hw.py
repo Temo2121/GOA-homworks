5c39e55eca0a4ceb7856a9ebc0740d2634907de2
Temo2121 <temodash25@gmail.com>
2025-07-17 17:38:06 +0400

    Update hw.py

---
+++ day 01/homework/hw.py
@@ -0,0 +1,14 @@
+# string არის ბრჭყალებში
+print("name")
+# integer არის მთელი რიცხვი
+print(16)
+# float არის ათწილადი რიცხვი
+print(1,75)
+# boolen True or Falsse
+print(True)
+
+#ცვალდი არიის ყუთში შენახული ინფომაცია
+
+name = "temo"
+age = 16
+height = 1,75
